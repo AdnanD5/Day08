c9d3398b17f8df0cb80179fda90bd72e526ff616
AdnanD5 <adodervisevic55@hotmail.com>
2021-03-15 22:55:51 +0100

    Dodavanje

---
+++ ex15/src/index.js
@@ -0,0 +1,9 @@
+// Only change code below this line
+function convertToInt(str, base){
+   return parseInt(str,base);
+}
+console.log(convertToInt("BA", 10));
+console.log(convertToInt("F1", 10));
+console.log(convertToInt("JeffBezos", 10));
+// Only change code above this line
+module.exports = convertToInt;
